d1d4e74e4277788195812162e17791c3eadd600a
Chartres Innovation <innovation@agglo-ville.chartres.fr>
2024-08-29 15:09:00 +0200

    Update app.py

--- app.py
+++ app.py
@@ -35,7 +35,7 @@ def transcribe_audio(file, api_key):
 
 
 
-st.title('Transcriber - Synthèse de réunion IA')
+st.title('Transcriber - Synthèse de réunion IA 🤖')
 
 # app container params
 with st.container():
